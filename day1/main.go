0842ab8f15bca3e3a8aa4648b08cfb7476baca8a
alouie-sfdc <arthur.louie@salesforce.com>
2022-05-30 15:35:36 -0700

    First part of day 1.

---
+++ day1/main.go
@@ -0,0 +1,45 @@
+// https://adventofcode.com/2021/day/1
+//
+// Reads measurements from an input file and calculates how many measurements are larger than the
+// previous measurements.
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const EMPTY_MEASUREMENT_SENTINEL_VALUE = -1
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: main.go FILENAME\n")
+		os.Exit(1)
+	}
+	filename := os.Args[1]
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file %s: %v\n", filename, err)
+		os.Exit(1)
+	}
+
+	previousMeasurement := EMPTY_MEASUREMENT_SENTINEL_VALUE
+	numIncreases := 0
+
+	for lineNumber, line := range strings.Split(string(data), "\n") {
+		currentMeasurement, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not convert value to integer on line %d: %s (%v)\n", lineNumber, line, err)
+			os.Exit(1)
+		}
+		if currentMeasurement > previousMeasurement && previousMeasurement != EMPTY_MEASUREMENT_SENTINEL_VALUE {
+			numIncreases += 1
+		}
+		previousMeasurement = currentMeasurement
+	}
+	fmt.Printf("Number of increases: %d\n", numIncreases)
+}
